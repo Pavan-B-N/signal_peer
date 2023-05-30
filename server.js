560d57279776ff8c24ca9b1791240a3709edb877
Pavan-B-N <pavangowda56005@gmail.com>
2023-05-30 18:44:09 +0530

    peer

---
+++ server.js
@@ -0,0 +1,25 @@
+const port = process.env.PORT || 3030;
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+const dotenv = require("dotenv")
+dotenv.config()
+
+const http = require("http")
+const httpServer = http.createServer(app)
+//webrtc
+const {ExpressPeerServer}=require("peer")
+const peerServer=ExpressPeerServer(httpServer)
+
+
+//Allowed origins
+const allowedOrigins = ["http://localhost:3000", "https://signalmessenger.netlify.app"];
+
+//middlewares
+app.use(express.json())
+app.use(cors({ origin: allowedOrigins }))
+app.use("/peerjs",peerServer);
+
+
+httpServer.listen(port, console.log(`server is running on port ${port} and origin is http://localhost:${port}`))
